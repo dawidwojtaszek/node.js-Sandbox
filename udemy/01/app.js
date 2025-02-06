ea2675e338a952ca5ff00abee5fa2d218fcbf1b7
Dawid <dawidw22@gmail.com>
2025-02-06 20:14:34 +0100

    notes

--- udemy/01/app.js
+++ udemy/01/app.js
@@ -1,38 +1,5 @@
 const http = require("http");
-const fs = require("fs");
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<body>");
-    res.write(
-      '<form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form>'
-    );
-    res.write("</body>");
-    res.write("</html>");
-    return res.end();
-  }
-
-  if (url === "/message" && method == "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody);
-      fs.writeFileSync("message.txt", parsedBody);
-    });
-
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-  }
-  return res.end();
-
-  res.setHeader("Content-Type", "text/html");
-  res.write("<h1>Hello huju</h1>");
-  res.end();
-});
+const requestHandler = require("./routes");
+const server = http.createServer(requestHandler);
 
 server.listen(4000); //Inicjalizacja serwera na porcie 3000
