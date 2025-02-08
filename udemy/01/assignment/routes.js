e01450d439c0b77a75a0b54462195eb0e29209db
Dawid <dawidw22@gmail.com>
2025-02-08 08:33:14 +0100

    finish solution

--- udemy/01/assignment/routes.js
+++ udemy/01/assignment/routes.js
@@ -1,8 +1,35 @@
 const requestHandler = (req, res) => {
   const url = req.url;
   const method = req.method;
+  console.log("new request", method);
+
+  if (url == "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write('<form method="POST" action="/create-user">');
+    res.write(
+      '<input name="create-user" type="text"> <button type="submit">Create User</button>'
+    );
+    res.write("</form>");
+    return res.end();
+  }
+
+  if (url === "/create-user" && method === "POST") {
+    const userNameUnparsed = [];
+    req.on("data", (chunk) => {
+      userNameUnparsed.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedUserName = Buffer.concat(userNameUnparsed).toString();
+      const userName = parsedUserName.split("=")[1];
+      console.log(userName);
+      res.statusCode = 301;
+      res.setHeader("Location", "/");
+      return res.end();
+    });
+  }
 
   if (url === "/users") {
+    res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<body>");
     res.write("<ul>Users list</ul>");
@@ -15,7 +42,7 @@ const requestHandler = (req, res) => {
   }
 
   res.setHeader("Content-Type", "text/html");
-  res.write("<h1>Home</h1>");
+  res.write("<html><body><h1>home</h1></body></html>");
   res.end();
 };
 
